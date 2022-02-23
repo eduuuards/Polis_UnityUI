6061438d227eb05810361add2f3844d0c603eb7e
Eduards <eduards.polis2@gmail.com>
2022-02-23 15:16:36 +0200

    Pievienots scrollview un paris pogas

--- Assets/Skripi/AtteluParadisana.cs
+++ Assets/Skripi/AtteluParadisana.cs
@@ -4,42 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class AtteluParadisana : MonoBehaviour {
-	public GameObject bins;
-	public GameObject lacis;
-	public GameObject tante;
-	public GameObject masina;
+	public GameObject pieauguso;
+	public GameObject bernu;
 	public GameObject attelaTuretajs;
 	public Sprite[] atteluMasivs;
 	public GameObject slaideris;
 
 
 
-	public void binaAttelosana(bool vertiba){
-		bins.SetActive (vertiba);
+	public void PieaugusoAttelosana(bool vertiba){
+		pieauguso.SetActive (vertiba);
 	}
-	public void lacisaAttelosana(bool vertiba){
-		lacis.SetActive (vertiba);
+	public void BernuAttelosana(bool vertiba){
+		bernu.SetActive (vertiba);
 	}
-	public void tantesAttelosana(bool vertiba){
-		tante.SetActive (vertiba);
-	}
-	public void masinasAttelosana(bool vertiba){
-		masina.SetActive (vertiba);
-	}
-
-	public void paKreisiBins(){
-		bins.transform.localScale = new Vector2 (1, 2);
-	}
-	public void paLabiBins(){
-		bins.transform.localScale = new Vector2 (-1, 2);
-	}
+		
 	public void izkritosais(int skaitlis){
 		if (skaitlis == 0)
 			attelaTuretajs.GetComponent<Image>().sprite = atteluMasivs [0];
 		else if (skaitlis == 1)
 			attelaTuretajs.GetComponent<Image>().sprite = atteluMasivs [1];
-		else if (skaitlis == 2)
-			attelaTuretajs.GetComponent<Image>().sprite = atteluMasivs [2];
 	}
 
 	public void mainitLielumu(){
