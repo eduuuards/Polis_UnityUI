7a4b2cce0fa13ccfdd63f5726c5505b215352426
Eduards <eduards.polis2@gmail.com>
2022-02-23 14:11:10 +0200

    Pievienota iespeja ievadit vecumu un vardu

--- Assets/Skripi/TekstaParadisana.cs
+++ Assets/Skripi/TekstaParadisana.cs
@@ -6,13 +6,16 @@ using UnityEngine.UI;
 public class TekstaParadisana : MonoBehaviour {
 	//Uzglabas tekstu kurs panemts no input lauka
 	public string teksts;
+	public string teksts2;
 	//Lauks no kura es to tekstu panemsu
 	public GameObject ievadesLauks;
+	public GameObject ievadesLauks2;
 	//teksta objekts kur attelot mainiga Teksta saturu
 	public GameObject tekstaAttelosanasLauks;
 
 	public void darbibasArTekstu(){
 		teksts = ievadesLauks.GetComponent<Text> ().text;
-		tekstaAttelosanasLauks.GetComponent<Text>().text = "Sveiks " + teksts.ToUpper () + "!";
+		teksts2 = ievadesLauks2.GetComponent<Text> ().text;
+		tekstaAttelosanasLauks.GetComponent<Text>().text = "Sis ir " + teksts.ToUpper () + "! Vinam ir " + teksts2.ToUpper() +" gadi.";
 	}
 }
